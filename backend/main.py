c7aef8484ec252d49b3cc0968a6cd377decaa334
shcats <shcats@gmail.com>
2022-12-14 23:54:58 +0300

    add user to DB

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from db import models
 from db.database import engine
+from routers import user
 
 app = FastAPI()
+app.include_router(user.router)
+
+
 
 @app.get("/")
 def root():
---
+++ backend/routers/user.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from routers.schemas import UserDisplay
+from sqlalchemy.orm.session import Session
+from .schemas import UserBase
+from db.database import get_db
+from db import user_db
+
+router = APIRouter(
+    prefix="/user",
+    tags=["user"]
+)
+
+@router.post("", response_model=UserDisplay)
+def create_user(request: UserBase, db:Session = Depends(get_db)):
+    return user_db.create_user(db=db, request=request)
