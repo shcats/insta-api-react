d8cd486c01f0a3ad5b252c7d21cfdcffece8ea7f
Alexander <shcats@gmail.com>
2022-12-23 14:32:21 +0300

    add hashing pass

--- backend/db/user_db.py
+++ backend/db/user_db.py
@@ -1,14 +1,16 @@
 from routers.schemas import UserBase
 from sqlalchemy.orm.session import Session
 from db.models import DbUser
+from .hashing import Hash
 
 def create_user(db:Session, request: UserBase):
     new_user =  DbUser(
         username = request.username,
         email = request.email,
-        password = request.password # hash password
+        password = Hash.bcrypt(request.password) # hash password
     )
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+    
