4da6889245fe12187a7c1f2e66ee26d83378167a
shcats <shcats@gmail.com>
2023-01-20 22:42:48 +0300

    React start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,25 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
+
+const BASE_URL = "http://127.0.0.1:8000"
 
 function App() {
+
+  const [post, usePost] = useState([])
+
+
+  useEffect(() => {
+    
+    fetch(BASE_URL + "/post/all")
+    .then((response)=>{
+      console.log(response.json());
+    })
+    .then()
+
+  }, [])
+  
+
   return (
     <div className="App">
       <p> Hekko</p>
